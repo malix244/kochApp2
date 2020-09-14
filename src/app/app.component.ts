db3436bac9173337f8d0f8d8377f20625e181b09
mbrueckn <maria-theresia.brueckner@tu-dresden.de>
2020-09-14 16:27:05 +0200

    Add Main-frame

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService} from './shared/api-service/api.service';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'KochApp';
+  img = 'pexels-lukas-349609.jpg';
 
   constructor(private apiService: ApiService) {
     this.apiService.loadRecipes().then(() => {
       console.log(this.apiService.getRecipes());
+      this.test();
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  private test(): void {
+    const tabsNewAnim = $('#navbarSupportedContent');
+    const selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+    const activeItemNewAnim = tabsNewAnim.find('.active');
+    let activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+    let activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+    let itemPosNewAnimTop = activeItemNewAnim.position();
+    let itemPosNewAnimLeft = activeItemNewAnim.position();
+
+    $('.hori-selector').css({
+      top: itemPosNewAnimTop.top + 'px',
+      left: itemPosNewAnimLeft.left + 'px',
+      height: activeWidthNewAnimHeight + 'px',
+      width: activeWidthNewAnimWidth + 'px'
+    });
+
+    $('#navbarSupportedContent').on('click', 'li', function(e): void {
+      $('#navbarSupportedContent ul li').removeClass('active');
+      $(this).addClass('active');
+      activeWidthNewAnimHeight = $(this).innerHeight();
+      activeWidthNewAnimWidth = $(this).innerWidth();
+      itemPosNewAnimTop = $(this).position();
+      itemPosNewAnimLeft = $(this).position();
+      $('.hori-selector').css({
+        top: itemPosNewAnimTop.top + 'px',
+        left: itemPosNewAnimLeft.left + 'px',
+        height: activeWidthNewAnimHeight + 'px',
+        width: activeWidthNewAnimWidth + 'px'
+      });
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const defaultRoute: Routes = [
+  {path: '', component: DashboardComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(defaultRoute),
   ],
   providers: [],
   bootstrap: [AppComponent]
