1beff3cf457e40adfcfc173b8111ed34072128b7
Mika <redscape244@yahoo.de>
2020-09-17 16:42:02 +0200

    new layout and function for filter

--- src/app/filter-search/filter-search.component.ts
+++ src/app/filter-search/filter-search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faHeart as fasHeart, faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as fasHeart, faSleigh, faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as farHeart, faStar as farStar, faClock, faUser } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
@@ -9,23 +9,30 @@ import { faHeart as farHeart, faStar as farStar, faClock, faUser } from '@fortaw
 })
 
 export class FilterSearchComponent implements OnInit {
+  [x: string]: any;
   farHeart = farHeart;
   fasHeart = fasHeart;
+  fasStar = fasStar;
+  farStar = farStar;
   farClock = faClock;
   farUsers = faUser;
 
 
   private favourite = false;
   heart = farHeart;
+  numbers: number[];
 
-  checkbox_vegetarian = false;
-  checkbox_vegan = false;
-  checkbox_meat = false;
-  checkbox_fish = false;
-  checkbox_main_dish = false;
-  checkbox_dessert = false;
+  checkboxVegetarian = false;
+  checkboxVegan = false;
+  checkboxMeat = false;
+  checkboxFish = false;
+  checkboxMainDish = false;
+  checkboxDessert = false;
 
-  constructor() { }
+
+  constructor() {
+    this.numbers = Array(10).fill(0).map((x, i) => i);
+  }
 
   ngOnInit(): void {
   }
@@ -40,12 +47,68 @@ export class FilterSearchComponent implements OnInit {
     }
   }
 
-  toggleEditable(event) {
-    if(event.target.checked) {
-      this.checkbox_dessert = true;
+  // tslint:disable-next-line: typedef
+  onSelectVegetarian(event) {
+    if (event.target.checked) {
+      document.getElementById('checkboxMeat').disabled = true;
+      document.getElementById('checkboxFish').disabled = true;
+    } else {
+      document.getElementById('checkboxMeat').disabled = false;
+      document.getElementById('checkboxFish').disabled = false;
     }
   }
 
+  // tslint:disable-next-line: typedef
+  onSelectVegan(event) {
+    if (event.target.checked) {
+      document.getElementById('checkboxMeat').disabled = true;
+      document.getElementById('checkboxFish').disabled = true;
+      document.getElementById('checkboxVegetarian').disabled = true;
+    } else {
+      document.getElementById('checkboxMeat').disabled = false;
+      document.getElementById('checkboxFish').disabled = false;
+      document.getElementById('checkboxVegetarian').disabled = false;
+    }
+  }
 
+  // tslint:disable-next-line: typedef
+  onSelectMeat(event) {
+    if (event.target.checked) {
+      document.getElementById('checkboxVegetarian').disabled = true;
+      document.getElementById('checkboxVegan').disabled = true;
+    } else {
+      document.getElementById('checkboxVegetarian').disabled = false;
+      document.getElementById('checkboxVegan').disabled = false;
+    }
+  }
+
+  // tslint:disable-next-line: typedef
+  onSelectFish(event) {
+    if (event.target.checked) {
+      document.getElementById('checkboxVegetarian').disabled = true;
+      document.getElementById('checkboxVegan').disabled = true;
+    } else {
+      document.getElementById('checkboxVegetarian').disabled = false;
+      document.getElementById('checkboxVegan').disabled = false;
+    }
+  }
+
+  // tslint:disable-next-line: typedef
+  onSelectMainDish(event) {
+    if (event.target.checked) {
+      document.getElementById('checkboxDessert').disabled = true;
+    } else {
+      document.getElementById('checkboxDessert').disabled = false;
+    }
+  }
+
+  // tslint:disable-next-line: typedef
+  onSelectDessert(event) {
+    if (event.target.checked) {
+      document.getElementById('checkboxMainDish').disabled = true;
+    } else {
+      document.getElementById('checkboxMainDish').disabled = false;
+    }
+  }
 
 }
